dcf005963e0309672488e31836612c3c94abc9b0
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2024-12-22 13:22:57 +0530

    Ray tracer project setup

---
+++ build/CMakeFiles/..dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "..exe"
+  "..exe.manifest"
+  "..pdb"
+  "CMakeFiles/..dir/main.cpp.obj"
+  "CMakeFiles/..dir/main.cpp.obj.d"
+  "lib..dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/..dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
